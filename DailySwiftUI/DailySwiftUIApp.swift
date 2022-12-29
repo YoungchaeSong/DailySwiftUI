4cf4baa9b3988085a948b5407d952d3c23a2bf59
송영채 <thddudco.dev@fitnesscandy.com>
2022-12-29 11:48:09 +0900

    Initial Commit

---
+++ DailySwiftUI/DailySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailySwiftUIApp.swift
+//  DailySwiftUI
+//
+//  Created by 송영채 on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct DailySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
